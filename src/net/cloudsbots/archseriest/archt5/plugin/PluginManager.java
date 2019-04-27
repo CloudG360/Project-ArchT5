a5514444e669b0541b94e3037f7378a888108cf2
CloudGamer360 <16578982+CloudGamer360@users.noreply.github.com>
2019-04-27 13:14:18 +0100

    Fixed Plugin Loading. It all works now.
    There was an issue with the way I loaded classes which lead to classes inheriting another class which hadn't been loaded throwing out errors. There's now a queue system in the idea that if a class is not present for a load but is in the queue, it will load. Generally the plugin loader should be more prepared for more complex plugins and should be a tad more organised for the eventual move over to a multi class loader plugin loader.

--- src/net/cloudsbots/archseriest/archt5/plugin/PluginManager.java
+++ src/net/cloudsbots/archseriest/archt5/plugin/PluginManager.java
@@ -4,6 +4,7 @@ import javafx.util.Pair;
 import net.cloudsbots.archseriest.archt5.Bot;
 import net.cloudsbots.archseriest.archt5.Main;
 import net.cloudsbots.archseriest.archt5.components.Logger;
+import net.cloudsbots.archseriest.archt5.components.Validator;
 import net.cloudsbots.archseriest.archt5.config.ConfigurationFile;
 import net.cloudsbots.archseriest.archt5.exceptions.*;
 
@@ -56,13 +57,31 @@ public final class PluginManager extends ClassLoader{
         }
     }
 
+    private String convertClassPath(String path, convertiontype outputtype){
+
+        String fpath = path;
+
+        switch(outputtype){
+            case PATH:
+                fpath = path.replace('.', '/')+".class";
+                break;
+            case CLASS:
+                fpath = path.replace('/', '.');
+                if(fpath.endsWith(".class")) {
+                    fpath = fpath.substring(0, fpath.length() - 6);
+                }
+                break;
+        }
+        return fpath;
+    }
+
     public HashMap<String, PluginPackaging> getPlugins(){
         return plgs;
     }
 
     public void loadPlugin(String pathToJar) throws Exception{
         try {
-            PluginLoadData plgloaddat = loadPlgClasses(pathToJar);
+            PluginLoadData plgloaddat = loadPluginClassesWithQueue(pathToJar);
             Class plgclass = plgloaddat.getMain();
             ConfigurationFile configurationFile = plgloaddat.getConfigurationFile();
             Object plg = plgclass.newInstance();
@@ -106,7 +125,121 @@ public final class PluginManager extends ClassLoader{
         }
     }
 
-    private PluginLoadData loadPlgClasses(String pathToJar) throws Exception {
+    private PluginJarData loadPlgConfig(JarFile jar, String pathToJar) throws IOException, InvalidConfigException, InvalidFormatException{
+
+        int entrieslist = 0;
+        Enumeration<JarEntry> jarentries = jar.entries();
+        InputStream plugincfg = null;
+        List<String> jarlisting = new ArrayList<>();
+        Logger.getLogger().logInfo("PluginManager/Load","Jar Entry Structure:");
+        while(jarentries.hasMoreElements()){
+            entrieslist++;
+            JarEntry entry = jarentries.nextElement();
+            jarlisting.add(entry.getName());
+            Logger.getLogger().logAppend("at -> ", entry.getName());
+            if(entry.getName().contains("plugin.cfgp")){
+                plugincfg = jar.getInputStream(entry);
+            }
+        }
+        if(plugincfg == null){ throw new InvalidPluginException("The jar at '"+pathToJar+"' is invalid as it does not contain a plugin.cfgp file."); }
+        Logger.getLogger().logInfo("PluginManager/Load", "Located plugin info! Loading info... ("+pathToJar+")");
+        BufferedReader r = new BufferedReader(new InputStreamReader(plugincfg));
+        return new PluginJarData(jarlisting, new ConfigurationFile(r, "id", "mainclass", "structure"));
+    }
+
+    private PluginLoadData loadPluginClassesWithQueue(String pathToJar){
+        if(!new File(pathToJar).exists()){ throw new PluginNotFoundException("The plugin jar at '"+pathToJar+"' doesn't exist. Aborting load."); }
+        try {
+            JarFile jar = new JarFile(new File(pathToJar));
+            Logger.getLogger().logInfo("PluginManager/Load", "Begging Load of plugin '" + pathToJar + "'");
+            Logger.getLogger().logInfo("PluginManager/Load", "Locating plugin info (" + pathToJar + ")");
+            PluginJarData jardat = loadPlgConfig(jar, pathToJar);
+            ConfigurationFile config = jardat.getPlgconfig();
+            List<String> jarlisting = jardat.getJarlisting();
+            Logger.getLogger().logInfo("PluginManager/Load", "Loaded info. Checks passed. Info is valid. ("+pathToJar+")");
+            String mainclasspath = (String) config.getConfig().get("mainclass");
+            String mainfilepath = convertClassPath(mainclasspath, convertiontype.PATH);
+            Logger.getLogger().logInfo("PluginManager/Load", "MainClassPath - "+mainclasspath+" MainFilePath - "+mainfilepath);
+            if(!jarlisting.contains(mainfilepath)) { throw new InvalidConfigException("Property 'mainclass' must point to a class - '" + mainfilepath+ "' is not present."); }
+            Logger.getLogger().logInfo("PluginManager/Load", "Beginning load of classes ("+pathToJar+")");
+
+            Class plgmain = null;
+            List<String> additionals = new ArrayList<>();
+
+            List<String> queue = new ArrayList<>();
+
+            for(String jarlisti:jarlisting){
+                if(jarlisti.endsWith(".class")){
+                    queue.add(jarlisti);
+                }
+            }
+
+            while(queue.size() > 0){
+                JarEntry entry = jar.getJarEntry(queue.get(0));
+                Logger.getLogger().logInfo("PluginManager/Load","Loading Class from queue '"+entry.getName()+"' ("+String.valueOf(queue.size()-1)+" left...");
+                InputStream classInput = jar.getInputStream(entry);
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                int data = classInput.read();
+                while(data != -1){
+                    buffer.write(data);
+                    data = classInput.read();
+                }
+                classInput.close();
+                byte[] classdat = buffer.toByteArray();
+                String location = entry.getName();
+                String path = convertClassPath(entry.getName(), convertiontype.CLASS);
+                queue.remove(0);
+                if(path.equals(mainclasspath)){
+                    Logger.getLogger().logInfo("PluginManager/Load","Main Class registering @ "+path);
+                    try {
+                        plgmain = defineClass(path, classdat, 0, classdat.length);
+                    } catch (NoClassDefFoundError err){
+                        if(queue.contains(err.getMessage()+".class")){
+                            queue.remove(err.getMessage()+".class");
+                            queue.add(0, err.getMessage()+".class");
+                            queue.add(1, location);
+                        } else {
+                            Logger.getLogger().logInfo("PluginManager/Load","Clearing queue. Required dependancy Main Class for @ "+path+" was not present. Aborting load.");
+                            queue.clear();
+                        }
+                    }
+                } else {
+                    Logger.getLogger().logInfo("PluginManager/Load","Additional Class registering @ "+path);
+                    try {
+                        defineClass(path, classdat, 0, classdat.length);
+                        additionals.add(path);
+                    } catch (NoClassDefFoundError err) {
+                        if(queue.contains(err.getMessage()+".class")){
+                            queue.remove(err.getMessage()+".class");
+                            queue.add(0, err.getMessage()+".class");
+                            queue.add(1, location);
+                        } else {
+                            Logger.getLogger().logInfo("PluginManager/Load","Clearing queue. Required dependancy for @ "+path+" was not present. Aborting load.");
+                            queue.clear();
+                        }
+                    }
+
+                }
+                classdat = null;
+            }
+
+            Logger.getLogger().logInfo("PluginManager/Load", "Cleaning Up. ("+pathToJar+")");
+            jar.close();
+            Logger.getLogger().logInfo("PluginManager/Load", "Loaded classes. Beginning Enable process. ("+pathToJar+")");
+            return new PluginLoadData(plgmain, additionals, config);
+        } catch (IOException err) {
+            throw new RethrownException(err, false);
+        } catch (InvalidConfigException err) {
+            throw new RethrownException(err, false);
+        } catch (InvalidFormatException err) {
+            throw new RethrownException(err, false);
+        }
+
+    }
+
+    // -------------------------------------------------------
+
+    /*private PluginLoadData loadPlgClasses(String pathToJar) throws Exception {
         //TODO: Implement Plugin Loader
 
         if(!new File(pathToJar).exists()){ throw new PluginNotFoundException("The plugin jar at '"+pathToJar+"' doesn't exist. Aborting load."); }
@@ -132,6 +265,7 @@ public final class PluginManager extends ClassLoader{
             Logger.getLogger().logInfo("PluginManager/Load", "Located plugin info! Loading info... ("+pathToJar+")");
             BufferedReader r = new BufferedReader(new InputStreamReader(plugincfg));
             ConfigurationFile config = new ConfigurationFile(r, "id", "mainclass", "structure");
+            //--
             Logger.getLogger().logInfo("PluginManager/Load", "Loaded info. Checks passed. Info is valid. ("+pathToJar+")");
             String mainpath = (String) config.getConfig().get("mainclass");
             String[] mainsplit = mainpath.split(Pattern.quote("."));
@@ -141,6 +275,7 @@ public final class PluginManager extends ClassLoader{
             }
             reformatmainpath = reformatmainpath.substring(1);
             reformatmainpath = reformatmainpath.concat(".class");
+            //--
             if(!jarlisting.contains(reformatmainpath)) { throw new InvalidConfigException("Property 'mainclass' must point to a class - '" + reformatmainpath+ "' is not present."); }
             Logger.getLogger().logInfo("PluginManager/Load", "Beginning load of classes ("+pathToJar+")");
             // Loading of Classes is below -------------------------------------------------------------------------------------------------------------------------------------------
@@ -150,29 +285,21 @@ public final class PluginManager extends ClassLoader{
             while(classentries.hasMoreElements()){
                 JarEntry entry = classentries.nextElement();
                 if(entry.getName().endsWith(".class")){
-                    Logger.getLogger().logInfo("PluginManager/Load","5");
                     InputStream classInput = jar.getInputStream(entry);
                     ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                    Logger.getLogger().logInfo("PluginManager/Load","6");
                     int data = classInput.read();
-                    Logger.getLogger().logInfo("PluginManager/Load","7");
                     while(data != -1){
                         buffer.write(data);
                         data = classInput.read();
                     }
-                    Logger.getLogger().logInfo("PluginManager/Load","8");
                     classInput.close();
-                    Logger.getLogger().logInfo("PluginManager/Load","3");
                     byte[] classdat = buffer.toByteArray();
                     String p = entry.getName().replace('/', '.');
                     String path = p.substring(0, p.length()-6);
-                    Logger.getLogger().logInfo("PluginManager/Load","4");
                     if(path.equals(mainpath)){
-                        Logger.getLogger().logInfo("PluginManager/Load","2");
                         Logger.getLogger().logInfo("PluginManager/Load","Main Class registering @ "+path);
                         plgmain = defineClass(path, classdat, 0, classdat.length);
                     } else {
-                        Logger.getLogger().logInfo("PluginManager/Load","1");
                         Logger.getLogger().logInfo("PluginManager/Load","Additional Class registering @ "+path);
                         defineClass(path, classdat, 0, classdat.length);
                         additionals.add(path);
@@ -204,6 +331,21 @@ public final class PluginManager extends ClassLoader{
         } catch (InvalidFormatException e){
             throw new InvalidFormatException("An InvalidFormatException was thrown whilst loading'"+pathToJar+"' ("+e.getMessage()+")");
         }
+    }*/
+
+    private enum convertiontype{
+        CLASS, PATH
+    }
+
+    private class PluginJarData {
+        private List<String> jarlisting;
+        private ConfigurationFile plgconfig;
+        public PluginJarData(List<String> jarlisting, ConfigurationFile plgconfig){
+            this.jarlisting = jarlisting;
+            this.plgconfig = plgconfig;
+        }
+        public ConfigurationFile getPlgconfig() { return plgconfig; }
+        public List<String> getJarlisting() { return jarlisting; }
     }
 
     private class PluginLoadData {
